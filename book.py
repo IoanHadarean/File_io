69e5d32e3889008aa92c873ef51dfba6661d4832
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 11:01:01 +0000

    Added further comments for book.py

--- book.py
+++ book.py
@@ -1,10 +1,11 @@
+#Imports regular expressions and collections
 import re
 import collections
 
-#Opens the book.text and reads everything that has lowercases
+#Opens the book.text and reads everything as lowercases
 text = open('book.txt').read().lower()
 #Finds all the words that are not a white space(w) and that occur once or more(+)
 words = re.findall('\w+', text)
 #Prints the counter of the most common ten words that meet the requirements
-print(collections.Counter(words).most_common(10))
+print(collections.Counter(words).most_common(5))
 
