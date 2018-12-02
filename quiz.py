05e1001fb9b2b4570fd450383b1eedbea093316e
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 11:19:03 +0000

    Added absolute path for relative_data.txt

--- quiz.py
+++ quiz.py
@@ -1,5 +1,5 @@
-#Opens relative_data.txt
-f = open('files/relative_data.txt', 'r')
+#Opens relative_data.txt with an absolute path
+f = open('/home/ubuntu/workspace/files/relative_data.txt', 'r')
 #Reads through the lines as text(Note: readlines gets the lines as a list of strings)
 lines = f.read()
 #Closes the open file
