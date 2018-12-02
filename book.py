9b5f776621fc37525e379fb603332cd96bde9c96
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 11:09:59 +0000

    Added relative_data.txt to quiz.py

--- book.py
+++ book.py
@@ -6,6 +6,7 @@ import collections
 text = open('book.txt').read().lower()
 #Finds all the words that are not a white space(w) and that occur once or more(+)
 words = re.findall('\w+', text)
-#Prints the counter of the most common ten words that meet the requirements
+#Prints the occurencies of the most common ten words that meet the requirements
+#as a list of tuples
 print(collections.Counter(words).most_common(10))
 
--- quiz.py
+++ quiz.py
@@ -1,5 +1,5 @@
-#Opens data.txt
-f = open('data.txt', 'r')
+#Opens relative_data.txt
+f = open('files/relative_data.txt', 'r')
 #Reads through the lines as text(Note: readlines gets the lines as a list of strings)
 lines = f.read()
 #Closes the open file
