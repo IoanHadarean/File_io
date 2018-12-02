6c1af5aa63ecf3a422e5d544f3535f7dd2affeb6
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 10:58:23 +0000

    Added comments to code

--- book.py
+++ book.py
@@ -1,6 +1,10 @@
 import re
 import collections
 
+#Opens the book.text and reads everything that has lowercases
 text = open('book.txt').read().lower()
+#Finds all the words that are not a white space(w) and that occur once or more(+)
 words = re.findall('\w+', text)
+#Prints the counter of the most common ten words that meet the requirements
 print(collections.Counter(words).most_common(10))
+
--- quiz.py
+++ quiz.py
@@ -1,4 +1,8 @@
+#Opens data.txt
 f = open('data.txt', 'r')
+#Reads through the lines as text(Note: readlines gets the lines as a list of strings)
 lines = f.read()
+#Closes the open file
 f.close()
+#Prints the lines
 print(lines)
