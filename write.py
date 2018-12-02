983dfcaae75307de4c60856b3229f9ba0c5d0dde
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 16:18:01 +0000

    Joined lines at \n

--- write.py
+++ write.py
@@ -2,6 +2,8 @@
 f = open('newfile.txt', 'a')
 #Creating a list of strings
 lines = ['Hello', 'World', 'Welcome', 'To', 'File IO']
+#Join lines separated by \n
+text = '\n'.join(lines)
 #Write lines to file
-f.writelines(lines)
+f.writelines(text)
 f.close()
