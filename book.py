fa95af362d989b8da8268a5f8ce629373b38e585
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-02 11:03:01 +0000

    Book.py comments

--- book.py
+++ book.py
@@ -7,5 +7,5 @@ text = open('book.txt').read().lower()
 #Finds all the words that are not a white space(w) and that occur once or more(+)
 words = re.findall('\w+', text)
 #Prints the counter of the most common ten words that meet the requirements
-print(collections.Counter(words).most_common(5))
+print(collections.Counter(words).most_common(10))
 
